d9f58dbef5f776851067a43610b9cf599e392ea5
Maxwell Yinger <max.yinger@formidable.com>
2020-04-19 09:54:35 -0600

    unused var

--- src/ease/ease.test.ts
+++ src/ease/ease.test.ts
@@ -1,7 +1,7 @@
 import { ease } from './ease';
 
 describe('ease', () => {
-  it.each(Object.entries(ease))('%s([0, 1]) -> [0, 1]', (key, easeFn) => {
+  it.each(Object.entries(ease))('%s([0, 1]) -> [0, 1]', (_, easeFn) => {
     expect(Math.abs(Math.round(easeFn(0)))).toEqual(0);
     expect(Math.round(easeFn(1))).toEqual(1);
   });
