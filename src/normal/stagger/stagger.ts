93a5a6c74e06d1d3bd37fab02be2e0ec9fb7f213
Maxwell Yinger <max@Maxwells-iMac-Pro.local>
2020-04-21 09:19:07 -0600

    initial staggers, needs cleanup

--- src/normal/stagger/stagger.ts
+++ src/normal/stagger/stagger.ts
@@ -1,4 +1,4 @@
-import { clamp, NORMAL, Range } from 'calc';
+import { clamp, delta, NORMAL, Range, transform } from 'calc';
 import { emitter } from 'emitter';
 import { Normal } from '../types';
 
@@ -8,28 +8,49 @@ type Config<T> = {
   normal: Normal<T>;
 };
 
-const interpolator = <T>(config: Config<T>) => {
-  const stagger = Array.from({ length: config.length }, (_, i) =>
+export const range = <T>(config: Config<T>): Range => {
+  const duration = config.normal.duration();
+  const staggers = Array.from({ length: config.length }, (_, i) =>
     typeof config.stagger === 'number' ? config.stagger : config.stagger(i)
   );
 
+  return staggers.reduce<Range>(
+    (r, stagger) => {
+      return [Math.min(r[0], stagger), Math.max(r[1], stagger + duration)];
+    },
+    [staggers[0], staggers[0] + duration]
+  );
+};
+
+export const interpolator = <T>(config: Config<T>) => {
+  const staggers = Array.from({ length: config.length }, (_, i) =>
+    typeof config.stagger === 'number' ? config.stagger : config.stagger(i)
+  );
+  const duration = config.normal.duration();
+  const configRange = range(config);
+  const normalized = staggers.map<Range>((stagger) => [
+    stagger / delta(configRange),
+    (stagger + duration) / delta(configRange),
+  ]);
+
   return (p: number): T[] => {
     const clamped = clamp(p, NORMAL);
-    console.log(clamped, stagger);
-    return [];
+    return normalized.map<T>((stagger) => {
+      return config.normal.progress(transform(clamped, stagger, NORMAL));
+    });
   };
 };
 
 export const stagger = <T>(config: Config<T>): Normal<T[]> => {
   const stream = emitter<T[]>();
   const configured = {
-    range: [0, 1] as Range,
+    range: range(config),
     interpolator: interpolator(config),
   };
 
   return {
     duration: () => {
-      return 300;
+      return delta(configured.range);
     },
     progress: (p: number) => {
       const interpolated = configured.interpolator(p);
